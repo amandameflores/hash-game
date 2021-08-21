5fdf2d61547cba9ff3a3834c17981fe1bd410135
amandaflores <amandaflores.br@gmail.com>
2021-08-21 11:46:36 -0300

    init project

---
+++ script.js
@@ -0,0 +1,18 @@
+//Initial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''
+};
+
+let player = '';
+let warning = '';
+let playing = false;
+
+//Events
+document.querySelector('.reset').addEventListener('click', reset)
+
+//Functions
+function reset() {
+    
+};
